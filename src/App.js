0daea0b91e255b80a0bd74de58ad5fba97e1e89b
Todd Briley <todd.briley@outlook.com>
2017-05-21 18:35:32 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import { firebaseconfig } from './config';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(firebaseconfig);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
